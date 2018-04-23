6f475b6bdd308c24f8d98c94ca5edb6122b30a41
Chris Wooten <chrismwooten@gmail.com>
2018-04-23 15:36:01 -0600

    Comments for clarity in main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+//This allows us to have our browser dynamically update as we make changes.
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//Bootstrapping the root module for our application:  AppModule
 platformBrowserDynamic().bootstrapModule(AppModule);
